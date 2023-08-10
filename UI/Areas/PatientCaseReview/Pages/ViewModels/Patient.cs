4f1e0910f9fd1bd8b104f15dac6e5a101f4e7b37
Kenneth Fukizi <ken.fukizi@afrikancoder.com>
2023-08-10 11:34:27 +0200

    Patient dsetails view model

---
+++ UI/Areas/PatientCaseReview/Pages/ViewModels/Patient.cs
@@ -0,0 +1,16 @@
+﻿using UI.Areas.PatientCaseReview.Pages.ViewModels.Enums;
+
+namespace UI.Areas.PatientCaseReview.Pages.ViewModels
+{
+    public class Patient
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+    }
+}
