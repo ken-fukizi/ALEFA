9cc87c69666a5726ad342a16b750f10d1f8e703a
Kenneth Fukizi <ken.fukizi@afrikancoder.com>
2023-08-11 23:54:22 +0200

    Clinical data domain model wired up

--- UI/Areas/PatientCaseReview/Pages/ViewModels/ClinicalDataViewModel.cs
+++ UI/Areas/PatientCaseReview/Pages/ViewModels/ClinicalDataViewModel.cs
@@ -6,6 +6,7 @@
         public int Temperature { get; set; }
         public int HgCount { get; set;} 
         public DateTime? LastVisitDateTime { get; set; } 
-        public string[]? LastPrescriptions { get; set;}
+        public string PrescriptionValues { get; set; } = string.Empty; // Use single string for now
+        //public string[]? LastPrescriptions { get; set;} = PrescriptionValues.Split(',');
     }
 }
---
+++ UI/Domain/Models/ClinicalDataAggregate/ClinicalDataModel.cs
@@ -0,0 +1,37 @@
+﻿using SharedKernel;
+
+namespace UI.Domain.Models.ClinicalDataAggregate
+{
+    public class ClinicalDataModel : AggregateRoot
+    {
+        public static class Factory
+        {
+            public static ClinicalDataModel Create(Guid patientGuid, int temperature, int hgCount, DateTime? lastVisitDateTime, string lastPrescriptions) 
+            {
+                var newClinicalDataModel = new ClinicalDataModel
+                    (
+                        patientGuid: patientGuid, 
+                        temperature: temperature, 
+                        hgCount: hgCount, 
+                        lastVisitDateTime: lastVisitDateTime, 
+                        lastPrescriptions: lastPrescriptions    
+                    );
+                return newClinicalDataModel; 
+            }
+        }
+        public ClinicalDataModel(Guid patientGuid, int temperature, int hgCount , DateTime? lastVisitDateTime, string lastPrescriptions)
+        {
+            PatientGuid = patientGuid;
+            Temperature = temperature;
+            HgCount = hgCount;
+            LastVisitDateTime = lastVisitDateTime;
+            LastPrescriptions = lastPrescriptions;
+                      
+        }
+        public Guid PatientGuid { get; private set; }
+        public int Temperature { get; private set; }
+        public int HgCount { get; private set; }
+        public DateTime? LastVisitDateTime { get; private set; }
+        public string LastPrescriptions { get; private set; } = string.Empty;
+    }
+}
