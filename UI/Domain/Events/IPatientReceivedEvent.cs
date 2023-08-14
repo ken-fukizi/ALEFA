9d5f38a7603017f4601f2d7d257bf2eb883e067b
Kenneth Fukizi <ken.fukizi@afrikancoder.com>
2023-08-14 09:27:51 +0200

    Patient received event

---
+++ UI/Domain/Events/IPatientReceivedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace UI.Domain.Events
+{
+    public interface IPatientReceivedEvent
+    {
+        public Guid EventId { get; set; }
+        public Guid PatientGuid { get; set; }
+        public long PatientIdNumber { get; set;}
+    }
+}
